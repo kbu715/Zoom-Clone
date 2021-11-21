9a639f858a230a1f67c4d2eee75517cc2f2902ac
kbu715 <kbu715@gmail.com>
2021-11-21 23:02:45 +0900

    ✨ feat: Frontend setup

--- src/server.js
+++ src/server.js
@@ -2,6 +2,11 @@ import express from "express";
 
 const app = express();
 
-console.log("Welcome to Server 💙");
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
-app.listen(3060);
+const handleListen = () => console.log(`Listening on http://localhost:3060`);
+app.listen(3060, handleListen);
