25fddc20057d766ad95053c0688e05b729911818
kbu715 <kbu715@gmail.com>
2021-11-23 22:29:17 +0900

    ✨ feat: WebSockets in NodeJS

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import { WebSocketServer } from 'ws';
 import express from "express";
 
 const app = express();
@@ -9,4 +11,11 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3060`);
-app.listen(3060, handleListen);
+// app.listen(3060, handleListen);
+
+const httpServer = http.createServer(app);
+
+// http 서버 위에 webSocket(ws) 서버를 만들 수 있도록 함
+const wss = new WebSocketServer({ server: httpServer }); // This can handle http and ws protocol // This is not required
+
+httpServer.listen(3060, handleListen);
