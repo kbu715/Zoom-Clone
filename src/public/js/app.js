e1bd716ed964f8f3d3590f2ee97a73ae4ccc0934
kbu715 <kbu715@gmail.com>
2021-11-29 22:19:56 +0900

    ✨ feat: socket.io rooms

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -57,15 +57,24 @@ const socket = io();
 
 const welcome = document.getElementById('welcome');
 const form = welcome.querySelector('form');
+const room = document.getElementById("room");
 
-function backendDone(msgFromBackend) {
-  console.log(`The backend says: `, msgFromBackend);
+room.hidden = true;
+
+let roomName;
+
+function showRoom() {
+  welcome.hidden = true;
+  room.hidden = false;
+  const h3 = room.querySelector("h3");
+  h3.innerText = `Room ${roomName}`;
 }
 
 function handleRoomSubmit(event) {
   event.preventDefault();
   const input = form.querySelector("input");
-  socket.emit("enter_room", { payload: input.value }, backendDone);
+  socket.emit("enter_room", input.value, showRoom);
+  roomName = input.value;
   input.value = "";
 }
 
--- src/server.js
+++ src/server.js
@@ -63,15 +63,14 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
+  socket.onAny((event) => {
+    console.log(`Socket Event: ${event}`);
+  })
   socket.on("enter_room", (roomName, done) => {
-    console.log(roomName);
-    setTimeout(() => {
-      done("Hello from the backend")
-    }, 10000);
-
-    // or 
-
-    // setTimeout(done, 10000, "Hello");
+    console.log(socket.id);
+    socket.join(roomName);
+    console.log(socket.rooms);
+    done();
   });
 });
 
