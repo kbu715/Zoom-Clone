77a8f6642b351534307742188a7ba8d05b5d9b18
kbu715 <kbu715@gmail.com>
2021-12-05 20:37:10 +0900

    ✨ feat: Answers

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -259,17 +259,18 @@ camerasSelect.addEventListener("input", handleCameraChange);
 const welcome = document.getElementById("welcome");
 const welcomeForm = welcome.querySelector("form");
 
-async function startMedia() {
+async function initCall() {
   welcome.hidden = true;
   call.hidden = false;
   await getMedia();
   makeConnection();
 }
 
-function handleWelcomeSubmit(event) {
+async function handleWelcomeSubmit(event) {
   event.preventDefault();
   const input = welcomeForm.querySelector("input");
-  socket.emit("join_room", input.value, startMedia);
+  await initCall();
+  socket.emit("join_room", input.value);
   roomName = input.value;
   input.value = ""; 
 }
@@ -281,7 +282,7 @@ welcomeForm.addEventListener("submit", handleWelcomeSubmit);
 
 // Socket Code
 
-// Peer A
+// Peer A (Brave Browser)
 socket.on("welcome", async () => {
   const offer = await myPeerConnection.createOffer();
   myPeerConnection.setLocalDescription(offer);
@@ -290,9 +291,16 @@ socket.on("welcome", async () => {
 })
 
 
-// Peer B
-socket.on("offer", (offer) => {
-  console.log(offer);
+// Peer B (Firefox Browser)
+socket.on("offer", async (offer) => {
+  myPeerConnection.setRemoteDescription(offer);
+  const answer = await myPeerConnection.createAnswer();
+  myPeerConnection.setLocalDescription(answer);
+  socket.emit("answer", answer, roomName);
+})
+
+socket.on("answer", (answer) => {
+  myPeerConnection.setRemoteDescription(answer);
 })
 
 // RTC code
